a79301fb4435b1b27866b96739efefd147b44bca
Pranaavvvv <pranav.djsce24@gmail.com>
2025-02-11 23:50:28 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,34 +1,7 @@
-import { Html, useProgress } from "@react-three/drei"
+import { Html, useProgress } from "@react-three/drei";
 
 const CanvasLoader = () => {
-  const { progress, errors } = useProgress()
-
-  // Show error if loading fails
-  if (errors.length > 0) {
-    return (
-      <Html
-        as="div"
-        center
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <p
-          style={{
-            fontSize: 14,
-            color: "#F1F1F1",
-            fontWeight: 800,
-            marginTop: 40,
-          }}
-        >
-          Error loading 3D view
-        </p>
-      </Html>
-    )
-  }
+  const { progress } = useProgress();
 
   return (
     <Html
@@ -45,16 +18,15 @@ const CanvasLoader = () => {
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
+          color: "#F1F1F1", // Removed extra double quotes
           fontWeight: 800,
           marginTop: 40,
         }}
       >
-        {progress.toFixed(0)}%
+        {progress.toFixed(2)}%
       </p>
     </Html>
-  )
-}
+  );
+};
 
-export default CanvasLoader
-
+export default CanvasLoader;
